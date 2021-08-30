1fc3c4878b188857fe82f72881654894b9a35678
Emerson Costa <emerson.costa2785@gmail.com>
2021-08-29 22:11:00 -0300

    CRUD da Entidade JornadaTrabalho criado

---
+++ src/main/java/com/dio/live/controller/JornadaTrabalhoController.java
@@ -0,0 +1,40 @@
+package com.dio.live.controller;
+
+import com.dio.live.service.JornadaService;
+import com.dio.live.model.JornadaTrabalho;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+
+@RestController // API REST para manipulação de arquivos Json
+@RequestMapping("/jornada") // Mapeamento de Rotas da API
+public class JornadaTrabalhoController {
+    @Autowired
+    JornadaService jornadaService;
+
+    @PostMapping
+    public JornadaTrabalho createJornada(@RequestBody JornadaTrabalho jornadaTrabalho){
+        return jornadaService.saveJornada(jornadaTrabalho);
+    }
+
+    @GetMapping
+    public List<JornadaTrabalho> getJornadaList(){
+        return jornadaService.findAll();
+    }
+
+    @GetMapping("/{idJornada}")
+    public ResponseEntity<JornadaTrabalho> getJornadaByID(@PathVariable("idJornada") Long idJornada){
+        return ResponseEntity.ok(jornadaService.getById(idJornada).orElseThrow(() -> new NoSuchElementException("Not found!")));
+    }
+
+
+
+
+
+}
